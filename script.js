0a3047a9d7ab20b5b4a1c99d8be1fb7afc314fbe
sandeep <gautamsandeep2021@gmail.com>
2024-09-05 21:24:05 +0530

    small screen pagination and add previous and next button

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let container = document.querySelector(".container");
 let btnContainer = document.querySelector(".btn-container");
 let content = document.querySelector(".content");
+let previousBtn = document.querySelector(".previous-btn");
+let nextBtn = document.querySelector(".next-btn");
+let smallScreenBtn = document.querySelector(".small-screen-btn");
 const data = [];
 // Insert random image in data .......
-for (let index = 1; index <= 100; index++) {
+for (let index = 1; index <= 200; index++) {
   data.push(`https://picsum.photos/id/${index}/200/300`);
 }
 // No. of item per page .........
-let perPageItem = 8;
+let perPageItem = 16;
 // create button for show item per page
 for (let index = 0; index < data.length / perPageItem; index++) {
   let button = document.createElement("button");
@@ -42,17 +45,70 @@ function showItem() {
     });
   });
 }
+// change page function ..........................
+function changePage(btnNumber) {
+  buttons.forEach((button) => button.classList.remove("active"));
+  buttons.forEach((button) => {
+    if (button.innerText == btnNumber) {
+      button.classList.add("active");
+    }
+  });
+  sliceData = data.slice(
+    perPageItem * btnNumber - perPageItem,
+    perPageItem * btnNumber
+  );
+  showItem();
+  window.scroll(0, 0);
+}
 
 showItem();
+let btnNumber = 1;
 let buttons = document.querySelectorAll(".btn-container button");
 buttons[0].classList.add("active");
 btnContainer.addEventListener("click", (e) => {
-  window.scroll(0, 0);
-  let n = e.target.innerText;
-  let targetBtn = e.target;
-  // remove active class from all buttons
-  buttons.forEach((button) => button.classList.remove("active"));
-  targetBtn.classList.add("active");
-  sliceData = data.slice(perPageItem * n - perPageItem, perPageItem * n);
-  showItem();
+  btnNumber = e.target.innerText;
+  // changepage function call when user click the button
+  changePage(btnNumber);
+});
+// when user click on next button show next page......
+nextBtn.addEventListener("click", () => {
+  btnNumber = Number(btnNumber) + 1;
+  // changepage function call when user click the next button
+  changePage(btnNumber);
+});
+// when user click on previous button show previous page......
+previousBtn.addEventListener("click", () => {
+  if (btnNumber == 1) return;
+  btnNumber = Number(btnNumber) - 1;
+  // changepage function call when user click the previous button
+  changePage(btnNumber);
+});
+
+// small screen pagination logic  ...............
+let n = 2;
+smallScreenBtn.addEventListener("click", () => {
+  n = n + 1;
+  let smallperpage = 8;
+  sliceData = data.slice(smallperpage * n - smallperpage, smallperpage * n);
+  sliceData.forEach((item) => {
+    // create dummy structure of image .....
+    let imgPlaceholder = document.createElement("div");
+    imgPlaceholder.classList.add("imgPlaceholder");
+
+    let img = document.createElement("img");
+    img.classList.add("box");
+    img.src = item;
+
+    content.appendChild(imgPlaceholder);
+
+    img.addEventListener("load", () => {
+      imgPlaceholder.replaceWith(img);
+    });
+    img.addEventListener("error", () => {
+      let img = document.createElement("img");
+      img.classList.add("box");
+      img.alt = "Failed to load";
+      imgPlaceholder.replaceWith(img);
+    });
+  });
 });
